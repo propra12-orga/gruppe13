eff9d77053c8f6d2cee95accf6567138a0436f6d
KFC89 <Kwok-Fai.Cheung@hhu.de>
2012-04-30 17:22:27 +0200

    mehr Muh

--- src/helloworld.java
+++ src/helloworld.java
@@ -2,6 +2,12 @@ public class helloworld {
 	public static void main(String[] args) {
 
 		System.out.println("MUH");
+		System.out.println("MUH2");
+		System.out.println("MUH3");
+		System.out.println("MUH4");
+		System.out.println("MUH5");
+		System.out.println("MUH6");
+		System.out.println("MUH7");
 
 	}
 }
