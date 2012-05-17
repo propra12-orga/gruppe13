6fdbac45ff33679498d1178fc39a00c9bb8ca21d
masch212 <maximilian.schneiders@uni-duesseldorf.de>
2012-05-17 16:37:03 +0200

    Feld für Levelauswahl modifiziert (für Tests)

--- src/Tests/TestFigur.java
+++ src/Tests/TestFigur.java
@@ -1,19 +1,25 @@
 package Tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import SF.Feld;
 import SF.Figur;
 
 public class TestFigur {
 
+	// Feld initialisieren
+	// Falls du wissen willst, wie das Testfeld aussieht: StartBomberman Zeile
+	// 21 "random" zu "test" aendern und ausfuehren!
+	Feld f = new Feld(5, 5, 32, 32, "test");
+
 	@Test
 	public void test() {
 		Figur a = new Figur();
-		   a.setxPosition(5);
-		   int b = a.getxPosition();
-		   assertTrue(5==b);
+		a.setxPosition(5);
+		int b = a.getxPosition();
+		assertTrue(5 == b);
 	}
 
 }
