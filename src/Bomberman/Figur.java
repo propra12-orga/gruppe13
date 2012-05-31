14841ae116874d32397cadf0d3d77308624adb84
KFC89 <Kwok-Fai.Cheung@hhu.de>
2012-05-31 20:31:48 +0200

    Neu Strukturierung , kein Glaspane , keine Bombe , Multiplayer

---
+++ src/Bomberman/Figur.java
@@ -0,0 +1,92 @@
+package Bomberman;
+
+public class Figur {
+	/*
+	 * neues Object. Grundlage fuer Figuren Mutterklasse von Bomberman und
+	 * Monstern.
+	 */
+
+	private int xposition;
+	private int yposition;
+
+	public Figur() {
+	}
+
+	public Figur(int xpos, int ypos) {
+		this.xposition = xpos;
+		this.yposition = ypos;
+
+	}
+
+	public int getxPosition() {
+		return this.xposition;
+	}
+
+	public void setxPosition(int xpos) {
+		this.xposition = xpos;
+	}
+
+	public int getyPosition() {
+		return this.yposition;
+	}
+
+	public void setyPosition(int ypos) {
+		this.yposition = ypos;
+	}
+
+	public boolean feldfrei(int x, int y, FieldEntry[][] map) {
+		// ueberprueft, ob ein Feld frei ist
+		// Momentan sind alle Felder ausser der solid-Kacheln gueltig
+
+		return map[x][y].getWalk();
+	}
+
+	public void rechts(FieldEntry[][] map) {
+		int x = getxPosition() + 1;
+		boolean b = this.feldfrei(x, getyPosition(), map);
+		if (b == true) {
+			this.setxPosition(x);
+			if (map[getxPosition()][getyPosition()].getImage() == 3) {
+				JMenue.feld.setVisible(false);
+				Start.M.setVisible(true);
+			}
+		}
+	}
+
+	public void links(FieldEntry[][] map) {
+		int x = getxPosition() - 1;
+		boolean b = this.feldfrei(x, getyPosition(), map);
+		if (b == true) {
+			this.setxPosition(x);
+			if (map[getxPosition()][getyPosition()].getImage() == 3) {
+				JMenue.feld.setVisible(false);
+				Start.M.setVisible(true);
+			}
+		}
+	}
+
+	public void unten(FieldEntry[][] map) {
+		int y = getyPosition() + 1;
+		boolean b = this.feldfrei(getxPosition(), y, map);
+		if (b == true) {
+			this.setyPosition(y);
+			if (map[getxPosition()][getyPosition()].getImage() == 3) {
+				JMenue.feld.setVisible(false);
+				Start.M.setVisible(true);
+			}
+		}
+	}
+
+	public void oben(FieldEntry[][] map) {
+		int y = getyPosition() - 1;
+		boolean b = this.feldfrei(getxPosition(), y, map);
+		if (b == true) {
+			this.setyPosition(y);
+			if (map[getxPosition()][getyPosition()].getImage() == 3) {
+				JMenue.feld.setVisible(false);
+				Start.M.setVisible(true);
+			}
+		}
+	}
+
+}
---
+++ src/Bomberman/Start.java
@@ -0,0 +1,11 @@
+package Bomberman;
+
+public class Start {
+
+	public static JMenue M;
+
+	public static void main(String[] args) {
+		M = new JMenue("Bomberman-Menue");
+	}
+
+}
